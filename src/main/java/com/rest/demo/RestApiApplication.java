3828617e815035a99a2e4274d7fd58fd3ac2e35d
kethan-kumar <kethankumar.nasapu@gmail.com>
2022-01-20 15:38:45 -0400

    REST API | Springboot | v1.0.0

--- src/main/java/com/rest/demo/RestApiApplication.java
+++ src/main/java/com/rest/demo/RestApiApplication.java
@@ -1,9 +1,21 @@
 package com.rest.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "REST APIs",
+				version = "1.0.0",
+				description = "RESTful APIs implemented in Springboot.",
+				contact = @Contact(
+						name = "Kethan Kumar Nasapu",
+						url = "https://kethan-portfolio.firebaseapp.com/")))
 public class RestApiApplication {
 
 	public static void main(String[] args) {
